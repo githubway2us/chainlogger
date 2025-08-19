99b03c3fe8c00cb67649188a96e88f88e9137ec4
GEOMANss <61168846+githubway2us@users.noreply.github.com>
2025-08-19 09:35:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
-import psycopg2
+import psycopg
 import hashlib
 import secrets
 from datetime import datetime
